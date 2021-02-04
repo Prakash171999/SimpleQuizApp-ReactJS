77c0e71c1e0a5b59f8b7e3e29b78f45fcac08ad4
Prakash Rai <prr0154@my.londonmet.ac.uk>
2021-02-04 10:22:24 +0545

    event handling done.

--- src/App.js
+++ src/App.js
@@ -44,6 +44,21 @@ const App = () => {
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
 
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score+1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    }
+    else{
+      setShowScore(true);
+    }
+
+  }
+
   return(
     <div className="app">
       {showScore ? (
@@ -52,14 +67,14 @@ const App = () => {
         <>
         <div className="question-section">
           <div className="question-count">
-            <span>Question {currentQuestion + 1}</span>
+            <span>Question {currentQuestion + 1}</span>/{questions.length}
           </div>
           <div className="question-text">
             {questions[currentQuestion].questionText}
           </div>
           <div className="answer-section">
             {questions[currentQuestion].answerOptions.map((answerOption) => (
-              <button >{answerOption.answerText}</button>
+              <button onClick={()=>handleAnswerOptionClick(answerOption.isCorrect)} >{answerOption.answerText}</button>
             ))}
           </div>
         </div>
